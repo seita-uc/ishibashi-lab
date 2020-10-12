01029c30d1b02ae9efe4688ee28341fb7bcf0f6c
seita-uc <c201552k@gmail.com>
2020-10-12 15:39:29 +0900

    :watermelon: Add: basic simulation

--- demo/index.js
+++ demo/index.js
@@ -1,45 +1,53 @@
 (async () => {
-  console.log("simulation started");
+  console.log("simulation started\n");
 
   const role = {
-    Manager: 1,
-    Evaluator: 1,
-    Worker: 2,
+    Manager: "manager",
+    Evaluator: "evaluator",
+    Worker: "worker",
   };
   const manager = {
+    reputation: 0,
     role: role.Manager,
     points: 0,
   };
   const evaluator = {
+    reputation: 0,
     role: role.Evaluator,
     points: 0,
   };
   const worker = {
+    reputation: 0,
     role: role.Worker,
     points: 0,
   };
 
   // 三段階評価
   const evaluation = {
-    Unknown: 0,
-    Unsatisfactory: 1,
-    Satisfactory: 2,
-    Excellent: 3,
+    Unknown: "unknown",
+    Unsatisfactory: "unsatisfactory",
+    Satisfactory: "satisfactory",
+    Excellent: "excellent",
   };
   const taskStatus = {
-    Created: 0,
-    InProgress: 1,
-    InReview: 2,
-    Completed: 3,
-    Pending: 4,
+    Created: "created",
+    InProgress: "in progress",
+    InReview: "in review",
+    Completed: "completed",
+    Pending: "pending ",
   };
   const task = {
     description: "demo task",
-    reward: {
-      worker: 100,
+    // task終了時の給料
+    payouts: {
+      manager: 100,
+      worker: 70,
       evaluator: 50,
     },
-    evaluation: evaluation.Unknown,
+    evaluation: {
+      worker: evaluation.Unknown,
+      manager: evaluation.Unknown,
+    },
     status: taskStatus.Created,
   };
 
@@ -52,17 +60,58 @@
   task.status = taskStatus.InReview;
   console.log("task submitted: ", task);
 
+  task.evaluation.worker = evaluation.Satisfactory;
+  console.log("evaluator evaluated task: ", task);
+
+  task.evaluation.manager = evaluation.Satisfactory;
+  console.log("worker evaluated manager: ", manager);
+
   task.status = taskStatus.Completed;
   console.log("task completed: ", task);
 
-  // TODO taskに給与も設定する
-  // taskをworkerが提出して
-  // evaluatorが評価して evaluator => worker
-  // workerがmanagerを評価して worker => manager
-  // managerがcompleteにする
-  //
+  // evaluationに応じてreputationが変動する
+  if (task.status == taskStatus.Completed) {
+    // workerのretupation反映
+    switch (task.evaluation.worker) {
+      case evaluation.Unsatisfactory:
+        worker.reputation -= task.payouts.worker;
+        break;
+      case evaluation.Satisfactory:
+        worker.reputation += task.payouts.worker;
+        break;
+      case evaluation.Excellent:
+        worker.reputation += task.payouts.worker * 1.5;
+        break;
+      default:
+    }
+
+    // managerのretupation反映
+    switch (task.evaluation.manager) {
+      case evaluation.Unsatisfactory:
+        manager.reputation -= task.payouts.manager;
+        break;
+      case evaluation.Satisfactory:
+        manager.reputation += task.payouts.manager;
+        break;
+      case evaluation.Excellent:
+        manager.reputation += task.payouts.manager * 1.5;
+        break;
+      default:
+    }
+
+    // 全員のpayout実施
+    worker.points += task.payouts.worker;
+    manager.points += task.payouts.manager;
+    evaluator.points += task.payouts.evaluator;
+  }
+
+  console.log("\n------result------\n");
+  console.log("worker: ", worker);
+  console.log("manager: ", manager);
+  console.log("evaluator: ", evaluator);
+
   // completeにした時点で給与が評価に従って支払われるがのちに時価総額を加味した金額にする
   // そうすることでevaluatorの評価にのみ左右されずにすむ
 
-  console.log("simulation done");
+  console.log("\nsimulation done");
 })();
